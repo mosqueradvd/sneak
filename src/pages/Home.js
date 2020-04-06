2b5293d2bf473fff43077d809cdfb5332ef08d99
David Mosquera <mosqueradavid@outlook.es>
2020-04-06 16:14:40 -0500

    Update files

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import getData from "../utils/getData";
+
+const Home = async () => {
+  const characters = await getData();
+  const view = `
+        <div class="Characters">
+        ${characters.results
+          .map(
+            (character) => `
+        <article class="Characters-item">
+            <a href="#/${character.id}/">
+            <h2>${character.name}</h2>
+            <img src="${character.image}" alt="${character.name}"/>
+            </a>
+          </article>
+        `
+          )
+          .join("")}
+        </div>
+    `;
+  return view;
+};
+
+export default Home;
